781cb69fe81c7c541d290d7cd6eff410fca88237
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2022-10-12 11:55:50 -0500

    v 0.1
    Creación GUI y flujo para crear una empresa

---
+++ Class/Empresa.py
@@ -0,0 +1,81 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import random
+from sklearn.model_selection import train_test_split
+from sklearn import neighbors
+from sklearn.metrics import accuracy_score
+from datetime import datetime
+import pickle
+
+class Empresa:
+    models = [] #[Clustering model, predictive model]
+    metricas = [] #[Inertia,Exactitud]
+    data = pd.DataFrame()
+    clusteringData = None #clusteringData tiene los resultados del modelo de CLustering (registro con su respectivo cluster)
+    
+    def __init__(self, nombreEmpresa, sectorEmpresa):
+        self.idEmpresa = random.randrange(0000,9999)
+        self.nombreEmpresa = nombreEmpresa
+        self.sectorEmpresa = sectorEmpresa
+        self.fechaCreacionUsuario = datetime.utcnow()
+    
+    def dataStatistics(self):
+        if not self.data.empty:
+            print(self.data.info())
+            print(self.data.describe())
+    
+    def preparacionData(self,dataframe):
+        data = dataframe
+        data['Casado']=data['Casado'].astype('category')
+        data['Carro']=data['Carro'].astype('category')
+        data['Alq_Prop']=data['Alq_Prop'].astype('category')
+        data['Sindicato']=data['Sindicato'].astype('category')
+        data['Sexo']=data['Sexo'].astype('category')
+        self.data = pd.get_dummies(data,columns=['Casado','Carro','Alq_Prop','Sindicato','Sexo'])
+         
+    def clusteringModel(self):
+        toClient = []
+        model = KMeans(n_clusters=5,max_iter=500)
+        model.fit(self.data)
+        centroides=pd.DataFrame(model.cluster_centers_, columns=self.data.columns.values)
+        tmp = self.data
+        tmp["Clusters"] = model.labels_
+        self.clusteringData = tmp
+        self.models.append(model)
+        self.metricas.append(model.inertia_)
+        
+    
+    def exportModels(self):
+        filehandler = open("Models.obj","wb")
+        pickle.dump(self.models,filehandler)
+        filehandler.close()
+    
+    def predictiveModel(self):
+        features = self.clusteringData.drop("Clusters", axis = 1)
+        predictions = self.clusteringData["Clusters"]
+        X_train, X_test, Y_train, Y_test = train_test_split(features, predictions, test_size=0.3, stratify=predictions)
+        model_Knn = neighbors.KNeighborsClassifier(n_neighbors = 1, metric='euclidean')
+        model_Knn.fit(X_train, Y_train)
+        Y_pred_knn = model_Knn.predict(X_test)      
+        exactitud = accuracy_score(Y_test, Y_pred_knn)
+        self.metricas.append(exactitud)
+        self.models.append(model_Knn)
+    
+    def toString(self):
+        return "Empresa ("+str(self.idEmpresa)+") "+self.nombreEmpresa+", enfocada a: "+self.sectorEmpresa+". Registrada en Calamita en la fecha: " + str(self.fechaCreacionUsuario)
+        
+          
+if __name__ == "__main__": #TODO: Remove when merging with GUI
+
+    #Ejemplo de logica
+    empresaEjemplo = Empresa("Calamita INC", "Analitica de datos")  #datafile = 
+    print(empresaEjemplo.toString())
+    empresaEjemplo.preparacionData("Data/Empleados.xlsx")
+    #empresaEjemplo.dataStatistics() Descripción estadistica de la data
+    empresaEjemplo.clusteringModel() 
+    empresaEjemplo.predictiveModel()
+    print(empresaEjemplo.clusteringData) 
+    print(empresaEjemplo.models) #Resultado esperado: [KMeans(max_iter=500, n_clusters=5), KNeighborsClassifier(metric='euclidean', n_neighbors=1)]
+    print(empresaEjemplo.metricas)
+    
---
+++ main.py
@@ -0,0 +1,54 @@
+from email import message
+from tkinter import *
+from tkinter import ttk
+
+from matplotlib.pyplot import title
+from Class.Empresa import Empresa
+from tkinter import messagebox
+
+try: 
+    empresa_usuario = None       
+    GUI = Tk()
+    GUI.title("Calamita")
+    GUI.geometry("1280x720")
+    frm = ttk.Frame(GUI, padding=10)
+    frm.grid()
+    ttk.Label(frm, text="Bienvenido a nuestro software de perfilamiento, Calamita").grid(column=0,row=0)
+    ttk.Label(frm, text="Registrarse").grid(column=0,row=1)
+   
+    def createEmpresa(nombre_empresa,sector_empresa):
+        name_obj = nombre_empresa.get()
+        sector_obj = sector_empresa.get()
+        if name_obj != "" and sector_obj!= "":      
+            nombre_empresa.config(state=DISABLED)
+            sector_empresa.config(state=DISABLED)
+            empresa_usuario = Empresa(name_obj, sector_obj)
+            messagebox.showinfo(message="Registro exitoso", title="Update")
+        else:
+            messagebox.showerror(message="Por favor llenar los datos necesarios", title="Warning")
+        
+    def openNewWindow():
+        # Toplevel object which will
+        # be treated as a new window
+        newWindow = Toplevel(GUI)
+        # sets the title of the
+        # Toplevel widget
+        newWindow.title("Registro empresa")
+        # sets the geometry of toplevel
+        newWindow.geometry("1280x720")
+        # A Label widget to show in toplevel
+        Label(newWindow,
+            text ="Datos de la empresa").pack()
+        Label(newWindow,text="Nombre empresa").pack()
+        nombre_empresa = ttk.Entry(newWindow)
+        nombre_empresa.pack()
+        Label(newWindow,text="Sector empresa").pack()
+        sector_empresa = ttk.Entry(newWindow)
+        sector_empresa.pack()
+        ttk.Button(newWindow,command=lambda: createEmpresa(nombre_empresa,sector_empresa),text="Enviar datos").pack()
+    
+    ttk.Button(frm,command=openNewWindow, text="Registro").grid(column=0,row=2)
+    GUI.mainloop()
+except Exception:
+    print("Error")
+    
